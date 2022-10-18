5ab10bf2b4de7616ebbd1c34c1b035e085fbd6c8
Baptiste PFEFFERKORN <bpfefferkorn@linksip.fr>
2022-10-19 00:42:08 +0200

    Part 9.5 done

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,9 +1,10 @@
-type PhysicalData = {
+export type PhysicalData = {
   height: number
   weight: number
 }
 
-type Bmi = number
+export type Bmi = number
+export type BmiInfo = string
 
 type bmiLevel = {
   low: number
@@ -27,11 +28,7 @@ const bmiLevels: BmiLevels = {
   obese: { low: 40.0, high: 700, info: "you're so fat" },
 }
 
-const parseArguments = (args: string[]): PhysicalData => {
-  if (process.argv.length != 4) throw Error("Please give just 2 arguments: height and weight")
-
-  const height = Number(args[2])
-  const weight = Number(args[3])
+export const checkArguments = (height: number, weight: number): PhysicalData => {
   if (isNaN(height) || isNaN(weight)) {
     throw Error("Please give your height in cm and your weight in kg")
   }
@@ -47,24 +44,34 @@ const parseArguments = (args: string[]): PhysicalData => {
   return { height, weight }
 }
 
-const calculateBmi = (height: number, weight: number): Bmi => {
+const parseCLIArguments = (args: string[]): PhysicalData => {
+  if (args.length != 4) throw Error("Please give just 2 arguments: height and weight")
+  const height = Number(args[2])
+  const weight = Number(args[3])
+
+  return checkArguments(height, weight)
+}
+
+export const calculateBmi = (height: number, weight: number): Bmi => {
   console.log(`Height: ${height}cm, Weight: ${weight}kg... Let's have a look to your BMI`)
   const bmi: number = weight / Math.pow(height * 0.01, 2)
 
   return bmi
 }
 
-const checkBmi = (bmi: Bmi): string => {
+export const checkBmi = (bmi: Bmi): BmiInfo => {
   for (const levelType in bmiLevels) {
     const type = levelType as bmiLevelType
     if (bmi >= bmiLevels[type].low && bmi <= bmiLevels[type].high) {
       return `You're ${type} (${bmiLevels[type].info})`
-    } 
+    }
   }
+
+  return "You're something but what??"
 }
 
 try {
-  const { weight, height } = parseArguments(process.argv)
+  const { weight, height } = parseCLIArguments(process.argv)
   const bmi: Bmi = calculateBmi(height, weight)
   console.log("Your bmi : ", bmi)
   console.log("That means that ", checkBmi(bmi))
--- index.ts
+++ index.ts
@@ -1,8 +1,28 @@
 import express from "express"
+import { checkArguments, checkBmi, calculateBmi, type Bmi, type BmiInfo } from "./bmiCalculator"
 const app = express()
+
+app.use(express.json())
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!")
+})
+
+app.get("/bmi", (req, res) => {
+  try {
+    const { height, weight } = checkArguments(Number(req.query.height), Number(req.query.weight))
+    const bmi: Bmi = calculateBmi(height, weight)
+    const bmiInfo: BmiInfo = checkBmi(bmi)
+
+    res.send({ height, weight, bmi, bmiInfo })
+  } catch (err) {
+    const message = "An error occured, please check your parameters"
+    if (err instanceof Error) {
+      res.status(400).json({ error: `malformatted parameters: ${err.message}` })
+    } else {
+      res.status(500).json({ error: message })
+    }
+  }
 })
 
 const PORT = process.env.PORT || 3000
