efde03e772400f7a0e949785ac49b337958b586b
Baptiste PFEFFERKORN <bpfefferkorn@linksip.fr>
2022-10-18 23:33:29 +0200

    Part 9.3 done

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,4 +1,10 @@
 type WeekHours = number[]
+type Target = number
+
+interface CalculateParams {
+  weekHours: WeekHours
+  target: Target
+}
 
 interface WeekHoursReport {
   periodLength: number
@@ -10,19 +16,31 @@ interface WeekHoursReport {
   average: number
 }
 
-const calculateExercices = (weekHours: WeekHours): WeekHoursReport => {
+const parseArgumentsCalculator = (args: string[]): CalculateParams => {
+  if (process.argv.length < 3) throw Error("Please give some hours per day")
+
+  const weekHours: WeekHours = process.argv.slice(2).map((d) => Number(d))
+
+  if (weekHours.some((d) => isNaN(d) || d > 24)) {
+    throw Error("Please give time per day in hours (max 24h)")
+  }
+  const target: Target = weekHours.shift()
+
+  return { target, weekHours }
+}
+
+const calculateExercices = (weekHours: WeekHours, target: Target): WeekHoursReport => {
   const periodLength = weekHours.length
   const trainingDays = weekHours.filter((d) => d > 0).length
   const average = weekHours.reduce((acc, d) => acc + d, 0) / periodLength
-  const target = 2
-  const rating = Math.round(average)
+  const rating = Math.round((average * 100) / target / 33)
   const success = rating > target
   const ratingDescription = [
     "so close to nothing",
     "better than nothing",
     "not to bad but could be better",
     "good job!",
-  ][rating]
+  ][rating <= 3 ? rating : 3]
 
   return {
     periodLength,
@@ -35,7 +53,14 @@ const calculateExercices = (weekHours: WeekHours): WeekHoursReport => {
   }
 }
 
-const myWeek: WeekHours = [3, 0, 2, 4.5, 0, 3, 1]
-
-const report = calculateExercices(myWeek)
-console.log(report)
+try {
+  const { target, weekHours } = parseArgumentsCalculator(process.argv)
+  const report = calculateExercices(weekHours, target)
+  console.log(report)
+} catch (err: unknown) {
+  if (err instanceof Error) {
+    console.log(err.message)
+  } else {
+    console.log("An error occured: ", err)
+  }
+}
